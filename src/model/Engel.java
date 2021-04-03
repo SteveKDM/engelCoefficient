a43a770c2be1955a0c02887ae253d21b0cea7cea
SteveKDM <shskkodama@yahoo.co.jp>
2021-04-04 06:46:02 +0900

    Add comma

--- src/model/Engel.java
+++ src/model/Engel.java
@@ -4,15 +4,21 @@ import java.io.Serializable;
 
 // エンゲル係数に関する情報を保持するJavaBeans
 public class Engel implements Serializable {
-	private int totalExpense;
-	private int foodExpense;
+	private long totalExpense;
+	private String totalExpenseComma;
+	private long foodExpense;
+	private String foodExpenseComma;
 	private double engelCoefficient;
 	private String adviceMsg;
 
-	public int getTotalExpense() { return totalExpense; }
-	public void setTotalExpense(int totalExpense) { this.totalExpense = totalExpense; }
-	public int getFoodExpense() { return foodExpense; }
-	public void setFoodExpense(int foodExpense) { this.foodExpense =foodExpense; }
+	public long getTotalExpense() { return totalExpense; }
+	public void setTotalExpense(long totalExpense) { this.totalExpense = totalExpense; }
+	public String getTotalExpenseComma() { return totalExpenseComma; }
+	public void setTotalExpenseComma(String totalExpenseComma) { this.totalExpenseComma = totalExpenseComma; }
+	public long getFoodExpense() { return foodExpense; }
+	public void setFoodExpense(long foodExpense) { this.foodExpense =foodExpense; }
+	public String getFoodExpenseComma() { return foodExpenseComma; }
+	public void setFoodExpenseComma(String foodExpenseComma) { this.foodExpenseComma = foodExpenseComma; }
 	public double getEngelCoefficient() { return engelCoefficient; }
 	public void setEngelCoefficient(double engelCoefficient) { this.engelCoefficient = engelCoefficient; }
 	public String getAdviceMsg() { return adviceMsg; }
--- src/model/EngelCalcLogic.java
+++ src/model/EngelCalcLogic.java
@@ -1,13 +1,21 @@
 package model;
 
+import java.text.NumberFormat;
+
 public class EngelCalcLogic {
 	public void execute(Engel engel) {
 
 		// エンゲル係数を算出して設定
-		int totalExpense = engel.getTotalExpense();
-		int foodExpense = engel.getFoodExpense();
+		long totalExpense = engel.getTotalExpense();
+		long foodExpense = engel.getFoodExpense();
 		double engelCoefficient = (double)Math.floor((double) foodExpense / totalExpense * 1000)/10;
 		engel.setEngelCoefficient(engelCoefficient);
+
+		// 「家計総支出」「食費支出額」をカンマ形式にして設定
+		String totalExpenseComma = NumberFormat.getInstance().format(totalExpense);
+		String foodExpenseComma = NumberFormat.getInstance().format(foodExpense);
+		engel.setTotalExpenseComma(totalExpenseComma);
+		engel.setFoodExpenseComma(foodExpenseComma);
 
 		// エンゲル係数からメッセージ（アドバイス）を設定
 		// 15-20%を適正とする
